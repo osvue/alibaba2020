846863bc5a83a7543e592a6666c2b1edca11c89d
thegif <gif@163.com>
2020-03-16 15:45:24 +0800

    03/16      gateway

---
+++ gateway8888/src/main/java/com/qiangzi/springcloud/gateway/GateApp.java
@@ -0,0 +1,23 @@
+package com.qiangzi.springcloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @program: alibaba2020
+ * @description: 网关服务
+ * @author: Mr.Han
+ * @create: 2020-03-16 11:44
+ **/
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GateApp {
+
+
+        public static void main(String[] args) {
+            SpringApplication.run(GateApp.class,args);
+        }
+
+    }
+ 
---
+++ gateway8888/src/main/java/com/qiangzi/springcloud/gateway/filter/GlobalLog.java
@@ -0,0 +1,44 @@
+package com.qiangzi.springcloud.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Date;
+
+/**
+ * @program: alibaba2020
+ * @description: 全局过滤器, 鉴权, 日志, 记录
+ * @author: Mr.Han
+ * @create: 2020-03-16 15:34
+ **/
+@Component
+@Slf4j
+public class GlobalLog implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("***********come in MyLogGateWayFilter:  " + new Date());
+
+        String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+
+        if (uname == null) {
+            log.info("*******用户名为null，非法用户，o(╥﹏╥)o");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
+ 
